dfe821ed090338ab38f3464a4491b3e720952cd5
Paulo Henrique Xavier Lopes <42495135+PauloHXL@users.noreply.github.com>
2023-08-29 00:53:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
 function Home() {
-    return <h1>Minha Cremosa, te amo!!!</h1>
+  return <h1>Minha Cremosa, te amo!!!</h1>;
 }
+
+function Teste() {
+  console.log("Teste");
+}
+
+function Teste2() {
+  console.log("Teste");
+}
+
 export default Home;
