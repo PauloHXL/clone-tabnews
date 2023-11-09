67253ccb487bccb5fddb3df919f87b4e296ac664
Paulo Henrique Xavier Lopes <42495135+PauloHXL@users.noreply.github.com>
2023-11-09 00:22:27 +0000

    add `.env` file

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    database: "postgres",
-    password: "local_password",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 function Home() {
   return <h1>Em construção...</h1>;
 }
-
-function Teste() {
-  console.log("Teste");
-}
-
-function Teste2() {
-  console.log("Teste");
-}
-
 export default Home;
